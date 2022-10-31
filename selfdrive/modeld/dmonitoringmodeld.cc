d4e05c95d13d6f2eb3e1d1edd13d40c9adbb4a15
hikee9123 <hikee9123@gmail.com>
2022-10-31 23:02:13 +0900

    test

--- selfdrive/modeld/dmonitoringmodeld.cc
+++ selfdrive/modeld/dmonitoringmodeld.cc
@@ -11,7 +11,7 @@
 
 ExitHandler do_exit;
 
-void run_model(DMonitoringModelState &model ) { //}, VisionIpcClient &vipc_client) {
+void run_model(DMonitoringModelState &model, VisionIpcClient &vipc_client) {
   PubMaster pm({"driverState"});
   SubMaster sm({"liveCalibration"});
   float calib[CALIB_LEN] = {0};
@@ -19,14 +19,14 @@ void run_model(DMonitoringModelState &model ) { //}, VisionIpcClient &vipc_clien
 
   VisionIpcBufExtra extra = {};
   VisionBuf *buf;
-  while (!do_exit) {
+  //while (!do_exit) {
 
-  //  buf = vipc_client.recv(&extra);
-  //  if (buf == nullptr) 
-  //  {
-  //     util::sleep_for(100);
-   //    continue;
-   // }
+    buf = vipc_client.recv(&extra);
+    if (buf == nullptr) 
+    {
+       util::sleep_for(100);
+       continue;
+    }
       
 
     sm.update(0);
@@ -38,7 +38,7 @@ void run_model(DMonitoringModelState &model ) { //}, VisionIpcClient &vipc_clien
     }
 
     double t1 = millis_since_boot();
-   // DMonitoringResult res = dmonitoring_eval_frame(&model, buf->addr, buf->width, buf->height, calib);
+    DMonitoringResult res = dmonitoring_eval_frame(&model, buf->addr, buf->width, buf->height, calib);
     double t2 = millis_since_boot();
 
     // send dm packet
@@ -46,28 +46,31 @@ void run_model(DMonitoringModelState &model ) { //}, VisionIpcClient &vipc_clien
     util::sleep_for(1000);
     printf("dmonitoring process: %.2fms, from last %.2fms\n", t2 - t1, t1 - last);
     last = t1;
-  }
+ // }
 }
 
 int main(int argc, char **argv) {
   //setpriority(PRIO_PROCESS, 0, -15);  ANDROID_PRIORITY_BACKGROUND
- // setpriority(PRIO_PROCESS, 0, 19);
+  //setpriority(PRIO_PROCESS, 0, 19);
 
   // init the models
   DMonitoringModelState model;
   dmonitoring_init(&model);
 
- // VisionIpcClient vipc_client = VisionIpcClient("camerad", VISION_STREAM_DRIVER, true);
- // while (!do_exit && !vipc_client.connect(false)) {
- //   util::sleep_for(100);
- // }
+  VisionIpcClient vipc_client = VisionIpcClient("camerad", VISION_STREAM_DRIVER, true);
+  while (!do_exit && !vipc_client.connect(false)) {
+    util::sleep_for(100);
+  }
 
   // run the models
- // if (vipc_client.connected) {
- //   LOGW("connected with buffer size: %d", vipc_client.buffers[0].len);
-    run_model(model);//, vipc_client);
- // }
+  if (vipc_client.connected) {
+    LOGW("connected with buffer size: %d", vipc_client.buffers[0].len);
+    run_model(model, vipc_client);
+  }
 
   dmonitoring_free(&model);
+
+
+
   return 0;
 }
