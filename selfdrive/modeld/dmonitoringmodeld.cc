b9b41f679802111bb668a00633bcd3b0573ccf7e
hikee9123 <hikee9123@gmail.com>
2022-10-31 22:16:16 +0900

    test

--- selfdrive/modeld/dmonitoringmodeld.cc
+++ selfdrive/modeld/dmonitoringmodeld.cc
@@ -36,7 +36,7 @@ void run_model(DMonitoringModelState &model, VisionIpcClient &vipc_client) {
 
     // send dm packet
     dmonitoring_publish(pm, extra.frame_id, res, (t2 - t1) / 1000.0, model.output);
-
+    Sleep(1);
     printf("dmonitoring process: %.2fms, from last %.2fms\n", t2 - t1, t1 - last);
     last = t1;
   }
